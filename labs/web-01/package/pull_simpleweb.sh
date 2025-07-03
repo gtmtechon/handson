2765a596787005a0c40232d4fd0037936bafca08
gtmtech <ux20me@gmail.com>
2025-07-03 15:11:24 +0900

    web08

---
+++ labs/web-01/package/pull_simpleweb.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/gtmtechon/handson/refs/heads/main/handson-web-pub-bas-01/app.js
+wget https://raw.githubusercontent.com/gtmtechon/handson/refs/heads/main/handson-web-pub-bas-01/index.html
+wget https://raw.githubusercontent.com/gtmtechon/handson/refs/heads/main/handson-web-pub-bas-01/package.json
